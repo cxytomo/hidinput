14c710bf0f5256ecb7662e62ce165bed66b8faad
tomo <cxytomo@gmail.com>
2015-04-30 17:26:27 +0800

    little change
    little change

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
 $(document).ready(function(){
 	var select = document.getElementById("item-npp")
 	, segmnt = select.value
-	, result = $("")
+	// , result = $("")
 	, addr = $("#sub-btn").attr("cmcc-ajax");
 	$.ajax({
 		url: addr,
 		statusCode: {404: function(){alert("Page not found!");}}		
 	}).done(function(data) {
-		var rest;
-		$.each(data.result, function(n,value){
-			var cont = "";
-			cont = "<li>"+value+"</li>";
-			licon += cont;
-		});
-		rest = $('ul').html(licon);
-		rest.attr('id','result');
-		$('.content').append(rest);
+		// var rest;
+		console.log("in");
+		// $.each(data.result, function(n,value){
+		// 	var cont = "";
+		// 	cont = "<li>"+value+"</li>";
+		// 	licon += cont;
+		// });
+		// rest = $('ul').html(licon);
+		// rest.attr('id','result');
+		// $('.content').append(rest);
 	});
 
 	select.onchange = function(){
